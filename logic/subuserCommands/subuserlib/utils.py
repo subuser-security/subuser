815f2950c5b07c764a45c94c02bfc3ef142b1ebd
peter1000 <peter.margo@gmx.de>
2014-02-15 10:08:22 -0200

    * Added module: utils.py
    * Refactor code and simplify usage of subprocess.check_call

---
+++ logic/subuserCommands/subuserlib/utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# This file should be compatible with both Python 2 and 3.
+# If it is not, please file a bug report.
+import subprocess
+
+def subprocessCheckedCall(args, **kwargs):
+ """ simplify subprocess.check_call in other code
+ """
+ try:
+  subprocess.check_call(args, **kwargs)
+ except subprocess.CalledProcessError:
+  sys.exit('Command failed: %s' % ' '.join(args))
