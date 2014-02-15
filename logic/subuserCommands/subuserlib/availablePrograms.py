a7815fcadf4b3c6aadc70db80b8a0867e4792789
Timothy <timothyhobbs@seznam.cz>
2014-02-15 22:14:00 +0000

    * subuser list available now includes information about each program in the listing output.

--- logic/subuserCommands/subuserlib/availablePrograms.py
+++ logic/subuserCommands/subuserlib/availablePrograms.py
@@ -2,8 +2,13 @@
 # This file should be compatible with both Python 2 and 3.
 # If it is not, please file a bug report.
 import paths
+import os
 
 def available(programName):
  """ Returns True if the program is available for instalation. """
- import os
  return os.path.exists(paths.getProgramSrcDir(programName))
+
+def getAvailablePrograms():
+ """ Returns a list of program's available for instalation. """
+ availableProgramsPath = paths.getAvailableProgramsPath()
+ return os.listdir(availableProgramsPath)
