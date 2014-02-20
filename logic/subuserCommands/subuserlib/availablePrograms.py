2234e34d05725b57a6604087bb3acef948ee7e4a
peter1000 <peter.margo@gmx.de>
2014-02-20 12:30:44 -0300

    getTextAvailablePrograms: for nicer user feedback
    similar to:  getTextInstalledPrograms: for nicer user feedback #96 
    
    ```
    ===== SAVE COMPRESSED DOCKER IMAGES =====
    
        elm-get-git-dev elm-git-dev emacs firefox firefox-flash firefox-java git irssi libelm-platform-git-dev libhaskell-platform libreoffice libx11 skype vim xterm
    workerm@notebook:~$ subuser save-compressed-images vimd
    
    ===== SAVE COMPRESSED DOCKER IMAGES =====
    
        elm-get-git-dev
        elm-git-dev
        emacs
        firefox
        firefox-flash
        firefox-java
        git
        irssi
        libelm-platform-git-dev
        libhaskell-platform
        libreoffice
        libx11
        skype
        vim
        xterm
    
    workerm@notebook:~$ 
    ```

--- logic/subuserCommands/subuserlib/availablePrograms.py
+++ logic/subuserCommands/subuserlib/availablePrograms.py
@@ -12,3 +12,23 @@ def getAvailablePrograms():
   """ Returns a list of program's available for instalation. """
   availableProgramsPath = paths.getAvailableProgramsPath()
   return os.listdir(availableProgramsPath)
+
+def getTextAvailablePrograms(addNewLine=False, indentSpaces=0):
+  """ Returns a text of sorted available program names.
+  Arguments:
+   - indentSpaces: can be set for nicer output especially togehter with: addNewLine
+   - addNewLine: if True each install program name starts at a new line
+  
+  e.g.: `print(getTextInstalledPrograms(addNewLine=True, indentSpaces=3))`
+  """
+  outText = ''
+  indentionString = ''
+  if indentSpaces > 0:
+    indentionString = ' ' * indentSpaces
+    
+  if addNewLine:
+    for program in sorted(getAvailablePrograms()):
+      outText = ''.join([outText, indentionString, program, '\n'])
+  else:
+    outText = indentionString + ' '.join(sorted(getAvailablePrograms()))
+  return outText
