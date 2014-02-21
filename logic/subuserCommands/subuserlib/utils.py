c0fa67b0b1688c658317432230208a1f93483b1c
Timothy <timothyhobbs@seznam.cz>
2014-02-21 17:36:47 +0000

    utils.py: Forgot to rename the parseCommandLineArgs function in the docs

--- logic/subuserCommands/subuserlib/utils.py
+++ logic/subuserCommands/subuserlib/utils.py
@@ -42,10 +42,10 @@ This function parses a list of command line arguments.
 It returns a tuple (preCheckProgramNameList, userOptionList) where preCheckProgramNameList is a list of names of programs and userOptionList is a list of options that have been passed.  Simply put, anything that that you pass in the seccond argument to this function is an option, everything else is added to the preCheckProgramNameList.
  
  e.g:
-  userProgramList = getUserCommandLine(sys.argv[1:], [])[0]
+  userProgramList = parseCommandLineArgs(sys.argv[1:], [])[0]
   
   commandOptionList = ['--from-cache']
-  userProgramList, userOptionList = getUserCommandLine(sys.argv[1:], commandOptionList)
+  userProgramList, userOptionList = parseCommandLineArgs(sys.argv[1:], commandOptionList)
   """
   preCheckProgramNameList = []
   userOptionList = []
