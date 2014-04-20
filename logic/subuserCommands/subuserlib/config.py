87bb111a0459fcd4e089dcdbf3fa9779ce55fe0f
Timothy <timothyhobbs@seznam.cz>
2014-04-20 21:02:50 +0000

    Refactor config.py

--- logic/subuserCommands/subuserlib/config.py
+++ logic/subuserCommands/subuserlib/config.py
@@ -7,18 +7,21 @@ import os,inspect,json
 #internal imports
 #import ...
 
+#The folowing is copied from paths.py in order to avoid a circular import.
 home = os.path.expanduser("~")
 
-def _getSubuserDir():
+def getSubuserDir():
   """ Get the toplevel directory for subuser. """
   return os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))) # BLEGH!
 
-def _getSubuserConfigPaths():
+##########
+
+def getSubuserConfigPaths():
   """ Returns a list of paths to config.json files in order that they should be looked in. """
-  _configsPaths = []
-  _configsPaths.append(os.path.join(home,".subuser","config.json"))
-  _configsPaths.append("/etc/subuser/config.json") # TODO how does this work on windows?
-  _configsPaths.append(os.path.join(_getSubuserDir(),"config.json"))
+  configFileInHomeDir = os.path.join(home,".subuser","config.json")
+  configFileInEtc = "/etc/subuser/config.json"
+  configFileInSubuserDir = os.path.join(getSubuserDir(),"config.json")
+  _configsPaths = [configFileInHomeDir,configFileInEtc,configFileInSubuserDir]
   configsPaths = []
   for path in _configsPaths:
    if os.path.exists(path):
@@ -30,27 +33,27 @@ def _addIfUnrepresented(identifier,path,paths):
   if not identifier in paths.keys():
     paths[identifier] = path
 
-def expandPathInConfig(path,config):
-  """ Expand the path of a given setting. """
+def _expandPathInConfig(path,config):
+  """ Expand the environment variables in a config settings value given that the setting holds a path. """
   config[path] = os.path.expandvars(config[path])
 
-def expandPathsInConfig(paths,config):
+def __expandPathsInConfig(paths,config):
   for path in paths:
-    expandPathInConfig(path,config)
+    _expandPathInConfig(path,config)
 
-def expandVarsInPaths(config):
+def _expandPathsInConfig(config):
   """ Go through a freshly loaded config file and expand any environment variables in the paths. """
-  os.environ["SUBUSERDIR"] = _getSubuserDir()
-  expandPathsInConfig(["bin-dir","installed-programs.json","user-set-permissions-dir","program-home-dirs-dir"],config)
+  os.environ["SUBUSERDIR"] = getSubuserDir()
+  __expandPathsInConfig(["bin-dir","installed-programs.json","user-set-permissions-dir","program-home-dirs-dir"],config)
 
 def getConfig():
   """ Returns a dictionary of settings used by subuser. """
-  configPaths = _getSubuserConfigPaths()
+  configPaths = getSubuserConfigPaths()
   config = {}
   for _configFile in configPaths:
     with open(_configFile, 'r') as configFile:
       _config = json.load(configFile)
       for identifier,setting in _config.iteritems():
         _addIfUnrepresented(identifier,setting,config)
-  expandVarsInPaths(config)
+  _expandPathsInConfig(config)
   return config
