39d1992d16c01523aac97b821237228b53fcc89a
Timothy <timothyhobbs@seznam.cz>
2014-04-21 13:58:45 +0000

    Fix: describe command no longer crashes when program is not installed

--- logic/subuserCommands/subuserlib/describe.py
+++ logic/subuserCommands/subuserlib/describe.py
@@ -13,37 +13,44 @@ def printInfo(program,showProgramStatus):
 
   if not subuserlib.availablePrograms.available(program):
     print(program + " does not exist.\n")
+    return
+
+  permissions = subuserlib.permissions.getPermissions(program)
+  print(program+":")
+  print(" Description: "+permissions["description"])
+  print(" Maintainer: "+permissions["maintainer"])
+
+  if not subuserlib.registry.isProgramInstalled(program):
+    print(" Installed: False")
   else:
-    permissions = subuserlib.permissions.getPermissions(program)
-    print(program+":")
-    print(" Description: "+permissions["description"])
-    print(" Maintainer: "+permissions["maintainer"])
+    print(" Installed: True")
     if showProgramStatus:
       print(" Running: "+str(subuserlib.dockerImages.isProgramRunning(program)))
       if "last-update-time" in permissions.keys():
         print(" Needs update: "+str(not permissions["last-update-time"] == registry[program]["last-update-time"]))
-    if subuserlib.permissions.getExecutable(permissions):
-      print(" Executable: "+subuserlib.permissions.getExecutable(permissions))
-    else:
-      print(" Is a library")
-    if subuserlib.permissions.getSharedHome(permissions):
-      print(" Shares it's home directory with: "+subuserlib.permissions.getSharedHome(permissions))
-    # TODO print dependencies.
-    if not subuserlib.permissions.getUserDirs(permissions)==[]:
-      print(" Has access to the following user directories: '~/"+"' '~/".join(subuserlib.permissions.getUserDirs(permissions))+"'")
-    if not subuserlib.permissions.getSystemDirs(permissions)==[]:
-      print(" Can read from the following system directories: '"+"' '".join(subuserlib.permissions.getSystemDirs(permissions))+"'")
-    if subuserlib.permissions.getX11(permissions):
-      print(" Can display X11 windows.")
-    if subuserlib.permissions.getGraphicsCard(permissions):
-      print(" Can access your graphics-card directly for OpenGL tasks.")
-    if subuserlib.permissions.getSoundCard(permissions):
-      print(" Has access to your soundcard, can play sounds/record sound.")
-    if subuserlib.permissions.getWebcam(permissions):
-      print(" Can access your computer's webcam/can see you.")
-    if subuserlib.permissions.getInheritWorkingDirectory(permissions):
-      print(" Can access the directory from which it was launched.")
-    if subuserlib.permissions.getAllowNetworkAccess(permissions):
-      print(" Can access the network/internet.")
-    if subuserlib.permissions.getPrivileged(permissions):
-      print(" Is fully privileged.  NOTE: Highly insecure!")
+
+  if subuserlib.permissions.getExecutable(permissions):
+    print(" Executable: "+subuserlib.permissions.getExecutable(permissions))
+  else:
+    print(" Is a library")
+  if subuserlib.permissions.getSharedHome(permissions):
+    print(" Shares it's home directory with: "+subuserlib.permissions.getSharedHome(permissions))
+  # TODO print dependencies.
+  if not subuserlib.permissions.getUserDirs(permissions)==[]:
+    print(" Has access to the following user directories: '~/"+"' '~/".join(subuserlib.permissions.getUserDirs(permissions))+"'")
+  if not subuserlib.permissions.getSystemDirs(permissions)==[]:
+    print(" Can read from the following system directories: '"+"' '".join(subuserlib.permissions.getSystemDirs(permissions))+"'")
+  if subuserlib.permissions.getX11(permissions):
+    print(" Can display X11 windows.")
+  if subuserlib.permissions.getGraphicsCard(permissions):
+    print(" Can access your graphics-card directly for OpenGL tasks.")
+  if subuserlib.permissions.getSoundCard(permissions):
+    print(" Has access to your soundcard, can play sounds/record sound.")
+  if subuserlib.permissions.getWebcam(permissions):
+    print(" Can access your computer's webcam/can see you.")
+  if subuserlib.permissions.getInheritWorkingDirectory(permissions):
+    print(" Can access the directory from which it was launched.")
+  if subuserlib.permissions.getAllowNetworkAccess(permissions):
+    print(" Can access the network/internet.")
+  if subuserlib.permissions.getPrivileged(permissions):
+    print(" Is fully privileged.  NOTE: Highly insecure!")
