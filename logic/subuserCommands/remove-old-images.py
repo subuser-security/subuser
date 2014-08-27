064bdd38e7020770076465f766a74d4a38ace295
Timothy <timothyhobbs@seznam.cz>
2014-08-27 21:32:30 +0000

    remove-old-images, untested but "complete"

--- logic/subuserCommands/remove-old-images.py
+++ logic/subuserCommands/remove-old-images.py
@@ -3,16 +3,13 @@
 # If it is not, please file a bug report.
 
 #external imports
-import sys,os,optparse
+import optparse
 #internal imports
-import subuserlib.availablePrograms,subuserlib.install,subuserlib.describe,subuserlib.commandLineArguments
+import subuserlib.classes.user
 
 def parseCliArgs():
-  usage = "usage: subuser %prog [options] PROGRAM_NAME(s)"
-  description = """Install a set of subuser programs.  To view a list of programs that can be installed try:
-
-$ subuser list available
-"""
+  usage = "usage: subuser %prog"
+  description = """ Remove old, no longer used, installed images.  Note, once you do this, you will no longer be able to return to previous configuration states with subuser update checkout."""
   parser=optparse.OptionParser(usage=usage,description=description,formatter=subuserlib.commandLineArguments.HelpFormatterThatDoesntReformatDescription())
   advancedOptions = subuserlib.commandLineArguments.advancedInstallOptionsGroup(parser)
   parser.add_option_group(advancedOptions)
@@ -20,14 +17,13 @@ $ subuser list available
 
 #################################################################################################
 
-options,userProgramList = parseCliArgs()
+user = subuserlib.classes.user.User()
 
-for program in userProgramList:
-  print(program)
-  subuserlib.install.installProgramAndDependencies(program, options.useCache)
+for installedImageId,installedImage in user.getInstalledImages().iteritems():
+  imageInUse = False
+  for subuser in user.getRegistry().getSubusers():
+    if subuser.getImageId() == installedImageId:
+      imageInUse = True
+  if not imageInUse:
+    installedImage.removeDockerImage()
 
-print("\n============= INSTALLATION SUCCESSFULL =============\n\n")
-for program in userProgramList:
-  print("\n"+program+": has been installed with the following permissions.")
-  subuserlib.describe.printInfo(program,False)
-  print("You can change this program's permissions at any time by editing it's permissions.json file.")
