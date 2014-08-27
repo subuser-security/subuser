d556f7ac6463c76b6d843c8139e0742e58c7d482
Timothy <timothyhobbs@seznam.cz>
2014-08-27 21:41:29 +0000

    repair: first draft

--- logic/subuserCommands/repair.py
+++ logic/subuserCommands/repair.py
@@ -3,44 +3,25 @@
 # If it is not, please file a bug report.
 
 #external imports
-import sys,optparse
+import optparse
 #internal imports
-import subuserlib.registry,subuserlib.dockerImages,subuserlib.install,subuserlib.subprocessExtras,subuserlib.commandLineArguments
+import subuserlib.classes.user,subuserlib.verify
 
 ####################################################
 def parseCliArgs():
   usage = "usage: subuser %prog [options]"
-  description = """ Install all of the programs that were installed on a previous system, given that you backed up the installed-programs.json file.
+  description = """
+Repair your subuser installation.
 
-Your installed-programs.js file must be in the right place.  It should probably be in ~/subuser/.  If you have a ~/.subuser/config.json file, or an /etc/subuser/config.json file, then you should look in these files to find out where installed-programs.json should reside.
-
+This is usefull when migrating from one machine to another.  You can copy your ~/.subuser folder to the new machine and run repair, and things should just work.
 """
   parser = optparse.OptionParser(usage=usage,description=description,formatter=subuserlib.commandLineArguments.HelpFormatterThatDoesntReformatDescription())
   advancedInstallOptions = subuserlib.commandLineArguments.advancedInstallOptionsGroup(parser)
   parser.add_option_group(advancedInstallOptions)
   return parser.parse_args()
 
-def findOutWhatShouldBeInstalledButIsnt(programsThatAreSupposedToBeInstalled):
-  toInstall=[]
-  for program in programsThatAreSupposedToBeInstalled:
-    if not subuserlib.dockerImages.isProgramsImageInstalled(program):
-      toInstall.append(program)
-  return toInstall
-
-def installFromRegistry(useCache):
-  """ Installs all of the programs listed in installed-programs.json file. """
-  programsThatAreSupposedToBeInstalled = subuserlib.registry.getInstalledPrograms()
-  programsToInstall = findOutWhatShouldBeInstalledButIsnt(programsThatAreSupposedToBeInstalled)
-  print("The following programs will be installed.")
-  for program in programsToInstall:
-    print(program)
-  if not raw_input("Continue? [y/N]").lower()=="y":
-    sys.exit()
-  for program in programsToInstall:
-    print("\n=== Installing "+program+" from installed-programs.json registry....\n")
-    subuserlib.install.installProgramAndDependencies(program, useCache)
-
-#################################################################################################
-
 options,arguments=parseCliArgs()
-installFromRegistry(options.useCache)
+
+user = subuserlib.classes.user.User()
+subuserlib.verify.verify(user)
+user.getRegistry().commit()
