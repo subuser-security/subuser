59b878be5e56fa30beda0c492c153d19b0b15e32
Timothy Hobbs <timothyhobbs@seznam.cz>
2014-10-03 11:35:01 +0200

    Change mark-as-needing update to take permissions.json files rather than images as its arguments.

--- logic/subuserCommands/mark-as-updated.py
+++ logic/subuserCommands/mark-as-updated.py
@@ -4,16 +4,16 @@
 #external modules
 import optparse,sys
 #internal modules
-import subuserlib.installTime,subuserlib.commandLineArguments,subuserlib.resolve,subuserlib.classes.user
+import subuserlib.installTime,subuserlib.commandLineArguments,subuserlib.classes.permissions,subuserlib.classes.user
 
 def parseCliArgs(sysargs):
-  usage = "usage: subuser %prog IMAGE_NAME(s)"
+  usage = "usage: subuser %prog permissions.json_file(s)"
   description = """
 Mark a image as needing to be updated.  Note, that this may mess up the formatting of it's permissions.json file.  This command is usefull mainly to the maintainers of subuser.  We use this command when we update a package or hear that it has been updated up stream.  The effect of this command, is that the image will be updated when the user issues `subuser update`. 
 
 EXAMPLE:
 
-    $ subuser mark-as-updated firefox@default
+    $ subuser mark-as-updated firefox/permissions.json 
 """
   parser=optparse.OptionParser(usage=usage,description=description,formatter=subuserlib.commandLineArguments.HelpFormatterThatDoesntReformatDescription())
   return parser.parse_args(args=sysargs[1:])
@@ -24,13 +24,14 @@ def markAsUpdated(sysargs):
   """
   Mark the given images as needing to be updated by setting their 'last-update-time' attributes to the current time.
 
-  >>> mark_as_updated = __import__("mark-as-updated")#import self
-  >>> mark_as_updated.markAsUpdated(["mark-as-updated","foo"])
+  >>> mark_as_updated = __import__("mark-as-updated")#import self, os
+  >>> import subuserlib.classes.user,os
+  >>> user = subuserlib.classes.user.User()
+  >>> foosPermissionsFilePath = os.path.join(user.getRegistry().getRepositories()["default"]["foo"].getSourceDir(),"permissions.json")
+  >>> mark_as_updated.markAsUpdated(["mark-as-updated",foosPermissionsFilePath])
 
    Here we test if the updated update time was actually saved correctly.  16 is the length of a standard formatted subuser image update time.
 
-  >>> import subuserlib.classes.user
-  >>> user = subuserlib.classes.user.User()
    >>> len(user.getRegistry().getRepositories()["default"]["foo"].getPermissions()["last-update-time"])
    16
 
@@ -38,22 +39,22 @@ def markAsUpdated(sysargs):
    >>> import subuserlib.git
    >>> subuserlib.git.runGit(["checkout","HEAD",user.getRegistry().getRepositories()["default"]["foo"].getPermissions().getWritePath()],cwd=user.getRegistry().getRepositories()["default"].getRepoPath())
 
-  If the image doesn't exist, we print a user friendly error message.
+  If the file doesn't exist, we print a user friendly error message.
 
-  >>> mark_as_updated.markAsUpdated(["mark-as-updated","non-existant-image"])
+  >>> mark_as_updated.markAsUpdated(["mark-as-updated","/non-existant-path/permissions.json"])
   Traceback (most recent call last):
-  SystemExit: Could not mark the image: non-existant-image as needing an update.  Image does not exist.
+  SystemExit: Could not mark the file /non-existant-path/permissions.json as needing an update.  File does not exist.
   """
   user=subuserlib.classes.user.User()
-  options,imageIds = parseCliArgs(sysargs)
+  options,permissionsFiles = parseCliArgs(sysargs)
 
-  for imageId in imageIds:
+  for permissionsFile in permissionsFiles:
     try:
-      image = subuserlib.resolve.resolveImageSource(user,imageId)
-      image.getPermissions()["last-update-time"] = subuserlib.installTime.currentTimeString()
-      image.getPermissions().save()
-    except KeyError:
-      sys.exit("Could not mark the image: "+imageId+" as needing an update.  Image does not exist.")
+      permissions = subuserlib.classes.permissions.Permissions(user,readPath=permissionsFile,writePath=permissionsFile)
+      permissions["last-update-time"] = subuserlib.installTime.currentTimeString()
+      permissions.save()
+    except IOError:
+      sys.exit("Could not mark the file "+permissionsFile+" as needing an update.  File does not exist.")
 
 if __name__ == "__main__":
   markAsUpdated(sys.argv)
