d2d2a3101d2d115e0efeb41768d1d7355f7cac70
Timothy Hobbs <timothyhobbs@seznam.cz>
2014-10-16 11:33:34 +0200

    Add warning about directory rename

--- logic/subuserCommands/pathConfig.py
+++ logic/subuserCommands/pathConfig.py
@@ -4,5 +4,9 @@ def getSubuserDir():
   """ Get the toplevel directory for subuser. """
   return os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))) # BLEGH!
 
+oldSubuserlibLocation = os.path.join(getSubuserDir(),"logic","subuserCommands","subuserlib")
+if os.path.exists(oldSubuserlibLocation):
+  sys.exit("It looks like you recently did a git pull.  We recently moved one of our source directories.  Unfortunately, git isn't capable of dealing with directory renaming so you'll have to help us out.  Please delete the directory:\n\n"+oldSubuserlibLocation)
+
 site.addsitedir(os.path.join(getSubuserDir(),"logic"))
 
