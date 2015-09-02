ad028a7dae44858fde1252f5c4863bc88b0cba33
Timothy <timothyhobbs@seznam.cz>
2015-09-02 23:22:47 +0200

    Remove dead code

--- logic/subuserlib/commandLineArguments.py
+++ logic/subuserlib/commandLineArguments.py
@@ -10,15 +10,6 @@ import optparse
 #internal imports
 #import ...
 
-# Note the --from-cache option is not currently supported.  A pull request implementing this flag for "update all", "repair" and "subuser add" would be greatly appreciated :) .
-def advancedInstallOptionsGroup(parser):
-  """
-  These are advanced installation options shared by several commands, install, update ect.
-  """
-  advancedOptions = optparse.OptionGroup(parser,"Advanced Options")
-  advancedOptions.add_option("--from-cache",action="store_true",default=False,dest="useCache",help="""Use the layer cache while building the image's image.  This is dangerous and therefore dissabled by default.  The layer cache caches certain commands used to build layers.  Since some commands such as "apt-get update" should not be cached we turn this option off by default.""")
-  return advancedOptions
-
 class HelpFormatterThatDoesntReformatDescription (optparse.HelpFormatter):
   """
   Format help with indented section bodies but don't reformat the description.
