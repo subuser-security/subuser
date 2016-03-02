710ab6d100b0fdc653069d5fc57c7ba425413a50
Timothy <timothyhobbs@seznam.cz>
2016-03-02 01:01:19 +0100

    When checking for the docker group check for dockerroot as well.

--- logic/subuserlib/docker.py
+++ logic/subuserlib/docker.py
@@ -39,8 +39,13 @@ For installation instructions see <https://www.docker.io/gettingstarted/#h_insta
 """)
 
   username = getpass.getuser()
-  if (not os.getuid() == 0) and (not username in grp.getgrnam("docker").gr_mem):
-    sys.exit("""Error: You are not a member of the docker group.
+  if not os.getuid() == 0:
+    try:
+      groupmembers = grp.getgrnam("docker").gr_mem
+    except KeyError:
+      groupmembers = grp.getgrnam("dockerroot").gr_mem
+    if not username in groupmembers:
+      sys.exit("""Error: You are not a member of the docker group.
 
 To learn how to become a member of the docker group please watch this video: <http://www.youtube.com/watch?v=ahgRx5U4V7E>""")
   return executable
